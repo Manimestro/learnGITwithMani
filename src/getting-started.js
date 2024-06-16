e7e96055fe622a3b309e38851ef9bcd5848ae7cd
manikanta <manikantatez@gmail.com>
2024-06-16 22:32:49 +0530

    getting started with DVCSs

---
+++ src/getting-started.js
@@ -0,0 +1,25 @@
+//getting started with version control systems (VCS)
+
+    // Version control is a system that tracks changes to a file or a set of files over time
+
+    // previously pople copying files into another directory and manage versions
+
+    // Revision Control System is popular vc
+    // RCS operates by storing patch sets, which are the differences between file versions, in a special format on disk. By accumulating these patches, RCS can reconstruct the state of any file as it existed at any given point in time.
+
+    // Centralized Version Control Systems (CVCSs) for cloud store
+    // take a file from cloud make changes and then send as revision
+    // in this only one developer has access to only one revision
+
+    // Distributed Version Control Systems (DVCSs) git etc
+    // clients do not just check out the latest snapshot of files; they mirror the entire repository, complete with its full history
+    // Developers can work independently, commit changes locally, and then synchronize and share their changes with others
+
+//History
+
+    // The Linux kernel, a significant open-source project, initially managed changes through patches and archived files from 1991 to 2002.
+    // In 2002, the project adopted BitKeeper, a proprietary Distributed Version Control System (DVCS).
+    // By 2005, disagreements between the Linux community and BitKeeper's commercial developers led to the revocation of BitKeeper's free status.
+    // Git was born from this initiative, incorporating lessons learned from BitKeeper.
+
+    // Now git developed by linux kernal community
