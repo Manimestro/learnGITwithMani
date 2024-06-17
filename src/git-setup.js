78d0e3f8c2ac67703b4298527d5541379fb6fc99
manikanta <manikantatez@gmail.com>
2024-06-17 12:52:58 +0530

    feat/git-config

---
+++ src/git-setup.js
@@ -0,0 +1,32 @@
+// after installing git
+
+// setup git envinorment which is git config
+        // config variables can be stored in three different locations
+
+        // system level ---> [path]/etc/gitconfig
+        // user level ---> ~/.gitconfig
+        // dir level  ----> .git/config
+
+        // git config user.name yourname
+        // git config user.email youremail
+
+        // options :
+            // --local
+            // --global
+            // --system
+
+        // git config --list --show-origin ---> to know all env varibles for git
+
+        //env vars
+        // init.defaultBranch ---> default branch name
+
+        // git config [key] ---> to Read values
+
+// how to make a repo to gitrepo
+
+// 1.git init -----> initialize an git repo using
+// 2.git clone [ssh/https url]-----> clone from the remote server
+
+
+
+
