97726651252cd89e45af1fb598ed96e16a3dac51
manikanta <manikantatez@gmail.com>
2024-06-18 22:40:23 +0530

    git unstage (restore)

--- src/commit-history.js
+++ src/commit-history.js
@@ -5,3 +5,17 @@
 // git log -[number of commits]
 
 // there are so many ways we can sort, filter, and even we can format and see the output
+
+//Undoing Things
+
+// git commit --amend ---> takes stashing area files and adds to prev commit
+// usage: when you forget something to include on commit you use use this to amend changes
+
+//unstaging
+
+// git reset HEAD [path] ---> unstage
+
+//using restore
+// git restore --stagging [path] --> stage to unstage
+// git restore [path] --> reset changes to last stage state || last  commit state
+
