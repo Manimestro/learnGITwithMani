784916e4a68f35860812f3ff824bdbc25d726bee
manikanta <manikantatez@gmail.com>
2024-06-18 22:08:58 +0530

    git log

---
+++ src/commit-history.js
@@ -0,0 +1,7 @@
+// see your commit history (revisions)
+
+// git log --> logs all commit history
+// git log --pretty=oneline -->
+// git log -[number of commits]
+
+// there are so many ways we can sort, filter, and even we can format and see the output
