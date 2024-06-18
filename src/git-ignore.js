fd7c809259464513efceeb34adec7caad5c85413
manikanta <manikantatez@gmail.com>
2024-06-18 21:19:17 +0530

    git ignoring

---
+++ src/git-ignore.js
@@ -0,0 +1,11 @@
+// git ignoring
+
+// git ignores only if it is not already tracked
+// .gitignore
+
+// [filename] ---> ignores all files with name
+// /[filename] ---> ignores all files with the path specified
+// [dirname]/  ----> to ignore folder
+// *.[extension] ---> ignores all extensions
+// [dir]/*.[extension] ---> pattern
+// [dir]/**/*.[extension] ---> pattern
