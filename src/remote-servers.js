516b795130fc1df8e240adf2e15762d070feea0d
manikanta <manikantatez@gmail.com>
2024-06-19 22:28:20 +0530

    remote servers (fetch, merge, pull , push ) basics

---
+++ src/remote-servers.js
@@ -0,0 +1,19 @@
+// remote servers
+
+// git clone [url]--> clone remote repo with .git dir and remote server url
+// remote name defaults to origin (it is url )
+// git remote -v --> shows all remote servers
+// git remote show [remote name] --> shows more info about remote server
+// git remote remove [name]
+// git remote rename [name]
+
+
+// fetching data
+
+// git fetch  ---> fetches all branches in remote server (wont sync with local branch)
+// git merge ---> sync branch with respective branches
+
+// git pull --> fetch and merge
+// git push --> push to remote server
+
+// git remote rename [old] [new]--> rename server address name
