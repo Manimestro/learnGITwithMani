70ae899750574e530dd57d85cfc1213fdd090931
manikanta <manikantatez@gmail.com>
2024-06-20 22:00:54 +0530

    git tagging (tag,del,push)

---
+++ src/tagging.js
@@ -0,0 +1,12 @@
+// tagging a commit ref
+
+// usage: if you want to mark up to a commit as a version then .. tags are usefull
+
+// git tag [tagname]  --> creates a tag
+// git tag -a [tagname] [commitid]  -m --> creates a tag annotation tag (with message)
+// git tag -d [tagname] --> delete tag
+// git push origin [tagname] --> push tags to server
+// git push origin --delete [tagname] --> delete tag on server
+// git checkout -b [new branch] [tagname] --> creates new branch of that tag version and switch to it
+
+
