a13dbb047765dcea776e161cd2d76305a93fa108
manikanta <manikantatez@gmail.com>
2024-06-22 22:10:24 +0530

    git branch management (rename,delete,logging)

--- src/branching_merging.js
+++ src/branching_merging.js
@@ -48,3 +48,23 @@
 
 // if we get conflicts we need to resolve them and need to stage them .
 
+// git log --oneline --graph --> this will show the merges history correctly
+
+// when we do merge the merge commit  holds the refernce of all the changes that are added on merge (if we remove that commit all changes are discarded)
+
+
+// git branch management
+
+// git push --set-upstream [branch name ]--> this will push changes to branch and also set tracking (we can use git push to push to that branch or git pull) if we set upstream
+// git branch -d [name] --> delete branch with are allready merged into HEAD branch
+// git branch -D [name] --> delete unmerged branches too
+// git branch --merged --> shows merged branches
+// git branch --not-merged --> shoes unmerged branches
+// git branch --all --> shows all the branches 
+
+
+// use case : when you want to rename a branch in local and push it to remote and delete old branch in remote
+// git branch -m [name] --> renames branch
+// git push origin [name] --> push renamed branch to remote
+// git branch -all --> see all remote and local branches
+// git push -d origin [old branch] --> delete branch on remote
