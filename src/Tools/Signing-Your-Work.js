5fb944633c973c0444f320a31b81af702842af2a
manikanta <manikantatez@gmail.com>
2024-07-04 20:19:19 +0200

    Signed commit using GPG

---
+++ src/Tools/Signing-Your-Work.js
@@ -0,0 +1,12 @@
+//! Signing work
+// usage: If you’re taking work from others on the internet and want to verify
+// that commits are actually from a trusted source
+
+//! GPG
+// this is a tool to generate private key for specially to sign the things
+// it generate a asymictric crptographic hash
+// gpg key-gen --> generate private key
+// set it in signing key configuration
+
+//! Signing Tags
+// git tag -s signedTag -m 'signed tag' --> tag a signed tag
