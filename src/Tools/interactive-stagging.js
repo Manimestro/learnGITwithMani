a679b54e0428b22c302adae975acd95f885919e7
manikanta <manikantatez@gmail.com>
2024-07-07 20:19:19 +0200

    stagging Interactive (stage,unstage,diff)

---
+++ src/Tools/interactive-stagging.js
@@ -0,0 +1,9 @@
+//! Interactive Stagging
+
+// !Staging and Unstaging Files
+// git add -i --> opens interactive mode for staging
+// > u --> for update
+// > enter file number --> to stage that file
+// > r --> for revert
+// > enter file number --> to unstage that file
+// > d --> to show diff of stagged to unstaged files
