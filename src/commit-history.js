48e1265536699af3798b388de6911cc5371fdf24
manikanta <manikantatez@gmail.com>
2024-07-08 20:19:19 +0200

    commit-history (date change)

--- src/commit-history.js
+++ src/commit-history.js
@@ -10,6 +10,7 @@
 
 // git commit --amend ---> takes stashing area files and adds to prev commit
 // usage: when you forget something to include on commit you use use this to amend changes
+// git commit --amend --no-edit --date "17/08/22 20:19:19 +0200" --> to change the time of commit
 
 //! unstaging
 
