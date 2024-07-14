13ce800103668ce6b1750932b4b9bb56c38c6096
manikanta <manikantatez@gmail.com>
2024-07-14 23:12:28 +0530

    git tools (revisions)

---
+++ src/Tools/Revision-selection.js
@@ -0,0 +1,12 @@
+// !Single Revisions
+// Short SHA-1
+// ex: d5e596f30106c4d5f326a4d48f264cecfaa4a194
+// we can refer to a commit with at least 4chars from 40char
+// the commit hash of 12chars in mostly unique in the objects dir
+// git show [commit]
+
+// !Branch References
+// if you see any last commit of a branch we can use
+// git show [branchname]
+// git show [commit]
+// here both are same
