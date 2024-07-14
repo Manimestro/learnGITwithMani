a52cdd87f82fc88bfd2b6b1903f1e8d2a552c795
manikanta <manikantatez@gmail.com>
2024-07-15 01:17:09 +0530

    revision (reflog)

--- src/Tools/Revision-selection.js
+++ src/Tools/Revision-selection.js
@@ -1,12 +1,19 @@
 // !Single Revisions
-// Short SHA-1
-// ex: d5e596f30106c4d5f326a4d48f264cecfaa4a194
-// we can refer to a commit with at least 4chars from 40char
-// the commit hash of 12chars in mostly unique in the objects dir
-// git show [commit]
+    // !Short SHA-1
+    // ex: d5e596f30106c4d5f326a4d48f264cecfaa4a194
+    // we can refer to a commit with at least 4chars from 40char
+    // the commit hash of 12chars in mostly unique in the objects dir
+    // git show [commit]
 
-// !Branch References
-// if you see any last commit of a branch we can use
-// git show [branchname]
-// git show [commit]
-// here both are same
+    // !Branch References
+    // if you see any last commit of a branch we can use
+    // git show [branchname]
+    // git show [commit]
+    // here both are same
+
+    // !RefLog Shortnames
+    // reflog stores all the info upto 2-3 months if any changes happend to HEAD
+    // git reflog --> shows history
+    // it is completly local cannot be sent to remote or integrate with it
+    // git show HEAD@{index}
+
