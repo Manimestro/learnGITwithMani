05afbe416e088e9f682f2bea6939e9e310d500b9
manikanta <manikantatez@gmail.com>
2024-07-18 20:19:19 +0200

    Signed work (commit, tags)

--- src/Tools/Signing-Your-Work.js
+++ src/Tools/Signing-Your-Work.js
@@ -1,6 +1,9 @@
 //! Signing work
 // usage: If you’re taking work from others on the internet and want to verify
 // that commits are actually from a trusted source
+// Signed commit is a proof that commit is made by you
+// proof is your private GnuPG key
+// you need to add public GnuPG key into hosting platform to verify
 
 //! GPG
 // this is a tool to generate private key for specially to sign the things
@@ -8,5 +11,10 @@
 // gpg key-gen --> generate private key
 // set it in signing key configuration
 
+//! signing Commits
+// git commit -S --> Create a Signed Commit
+
 //! Signing Tags
-// git tag -s signedTag -m 'signed tag' --> tag a signed tag
+// git tag -s [name] --> tag a signed tag
+
+
