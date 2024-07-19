5ddc2ef7afd03bf19c852b92a155e23af266cc61
manikanta <manikantatez@gmail.com>
2024-07-19 20:19:19 +0200

    rewriting history (rebase interactive)

--- src/Tools/rewriting-history.js
+++ src/Tools/rewriting-history.js
@@ -1,2 +1,9 @@
 // !Changing the Last Commit
 // git commit --amend --> for rewriting latest commits
+
+// !Changing Multiple Commit Messages
+// even furthur you want we can use git rebase -i
+// git rebase -i commit^ --> open an editor to change the commits
+// select what to do with commit
+// then exit editor and do what to want on commit
+// then rebase --continue --> to continue
