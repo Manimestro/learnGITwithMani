0e9fbbff3f287a90e001bdb2ff5e498253e956c6
manikanta <manikantatez@gmail.com>
2024-07-20 20:19:19 +0200

    Searching (grep,git log)

---
+++ src/Tools/Searching.js
@@ -0,0 +1,5 @@
+//! Searching
+
+// git grep -n [searchword] ---> prints all the files with matching string
+// git log -S [searchword] --> all commits that word is modified
+// git log -L :[functionName]:[filepath] --> this will show diff for all the commits when that function changes
