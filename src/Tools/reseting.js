ba505621d6f1984a8e3d70f8aac48919acc81bd9
manikanta <manikantatez@gmail.com>
2024-07-24 20:19:19 +0200

    reset (reset branch soft,mixed,hard)

--- src/Tools/reseting.js
+++ src/Tools/reseting.js
@@ -17,3 +17,16 @@
 // branch reference moves to lateset commit
 // and working dir will have the snapshot of HEAD
 
+//!  reseting
+// this is opposite to commiting
+// git reset HEAD~1 --> this moves the branch reference by 1 commit back
+// move head --> update index --> update working dir
+// git reset --soft HEAd~1 --> this moves HEAD and branch (kind of removing commit on branch)
+// and leave the work on index
+// git reset --mixed HEAD~1 --> this will check the HEAD and stagged files and update Index with HEAD changes
+// git reset --hard HEAD~1 --> this will update working dir with index files (modified files are also overwriten)
+
+// //! recap
+// Move the branch HEAD points to (stop here if --soft).
+// Make the index look like HEAD (stop here unless --hard).
+// Make the working directory look like the index.
