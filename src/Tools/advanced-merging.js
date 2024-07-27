5ceffad33b40ebc011e72d2457305d04a7563cba
manikanta <manikantatez@gmail.com>
2024-07-27 20:19:19 +0200

    git conflicts

---
+++ src/Tools/advanced-merging.js
@@ -0,0 +1,7 @@
+/*
+! Merge Conflicts
+before merging any branch better to keep working dir clean
+git merge --abort --> stops the merge 
+
+git merge -Xignore-space-change [branch] -->  ignores whitespace conflicts
+*/
